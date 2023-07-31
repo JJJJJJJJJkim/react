3a86ef16810817004d7dfc2660938e98eb9049ae
Tiger Water <ddddf123@gmail.com>
2023-07-31 13:20:49 +0900

    state 선언, 데이터 바인딩

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-  let post = 'post name'
+  let post = 'post name';
+
+  let [postTitle, b] = useState(['와인 추천','한강 데이트','강아지 산책']);
+  
+  let [logo, setlogo] = useState('Vinhos');
 
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>blog</h4>
+        <h4>{logo}</h4>
       </div>
-        <h4 style={{color : 'navy', fontSize : '30px'}}>{ post }</h4>
+        <div className="list">
+          <h4>{postTitle[0]}</h4>       
+          <p>7월 31일 발행</p>
+        </div><div className="list">
+          <h4>{postTitle[1]}</h4>       
+          <p>8월 01일 발행</p>
+        </div>
+        <div className="list">
+          <h4>{postTitle[2]}</h4>       
+          <p>8월 02일 발행</p>
+        </div>
     </div>
   );
 }
