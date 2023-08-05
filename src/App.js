1e2641325fd31fd34b78d1121d3d1350d7727e53
Tiger Water <ddddf123@gmail.com>
2023-08-05 23:41:22 +0900

    글쓰기 버튼과 글삭제 추가

--- src/App.js
+++ src/App.js
@@ -6,53 +6,82 @@ import { useState } from 'react';
 
 function App() {
 
-  let post = 'post name';
+  const post = 'post name';
+  const [postTitle, setPostTitle] = useState(['와인 추천', '한강 데이트', '강아지 산책']);
+  const [title, setTitle] = useState(0);
+  const [logo, setlogo] = useState('Vinhos');
+  const [good, setGood] = useState([0, 0, 0]);
+  const [isModal, setModal] = useState(false);
 
-  let [postTitle, b] = useState(['와인 추천','한강 데이트','강아지 산책']);
-  let [title, setTitle] = useState(0);
-  let [logo, setlogo] = useState('Vinhos');
-  
-  let [good, jj] = useState([0,0,0]);
-  let [modal, setModal] = useState(false);
-  
-  
+
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>{ logo }</h4>
+        <h4>{logo}</h4>
       </div>
-        <button onClick={()=>{
-          let copy = [...postTitle];
-          copy.sort();
-          b(copy);
-        }}>가나다순정렬</button>
-        {
-          postTitle.map((a, i)=>{
-            return (
-              <div className="list" key={i}>
-                <h4 onClick={()=>{setTitle(i); setModal(true)}}>{ postTitle[i] }<span onClick={()=>{
-                    let copy = [...good];
-                    copy[i] +=1;
-                    jj(copy);
-                  }}>👍</span> { good[i] }</h4>
-                <p>8월 02일 발행</p>
-              </div>
-            )
-          })
-        }
-        
-        {
-           modal == true ? <Modal title={title} postTitle={postTitle} color={'pink'}/> : null
-        }
+      <button onClick={() => {
+        const copy = [...postTitle];
+        copy.sort();
+        setPostTitle(copy);
+      }}>가나다순정렬</button>
 
+      {
+        postTitle.map((a, i) => {
+          return (
+            <div className="list" key={i}>
+              <h4 onClick={() => {
+                setTitle(i); setModal(true);
+              }}>{postTitle[i]}
+                <span onClick={(e) => {
+                  const copy = [...good];
+                  copy[i] += 1;
+                  setGood(copy);
+                  e.stopPropagation();
+                }}>👍</span> {good[i]}<button onClick={(e)=>{
+                  const copy = [...postTitle];
+                  const goodCopy = [...good];
+
+                  copy.splice(i, 1);
+                  setPostTitle(copy);
+                  e.stopPropagation();
+                  isModal == true ? setModal(false) : null;
+
+                  goodCopy.splice(i, 1);
+                  setGood(goodCopy);
+                }}>글삭제</button></h4>
+              <p>8월 02일 발행</p>
+            </div>
+          )
+        })
+      }
+
+      {
+        isModal == true ? <Modal title={title} postTitle={postTitle} color={'pink'} /> : null
+      }
+
+      <label>
+        <input></input>
+        <button onClick={(e)=>{
+          const newPostTitle = e.target.previousSibling.value;
+          const copy = [...postTitle];
+          const goodCopy = [...good];
+
+          copy.push(newPostTitle);
+          setPostTitle(copy);
+          
+          goodCopy.push(0);
+          setGood(goodCopy);
+        }}>글쓰기</button>
+      </label>
+      
     </div>
   );
 }
 
-function Modal(props){
+function Modal(props) {
   return (
-    <div className='modal' style={{background : props.color}}>
+    <div className='modal' style={{ background: props.color }}>
       <h4>{props.postTitle[props.title]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
